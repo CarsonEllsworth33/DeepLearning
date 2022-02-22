048f4d1c9d3ce523fb7b77b7b20867974cbd47c7
Carson Ellsworth <ellsc33@yahoo.com>
2022-02-22 12:13:49 -0600

    NB code working, need to either choose a diff data set or modify it to work with NB better

--- Projects/P1/NB_P1.py
+++ Projects/P1/NB_P1.py
@@ -4,6 +4,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
+import math
+
+# Possible Bug: X_test has an Xi that is not in the X_train set due to using real nums, prob need to filter nums etc
+
 
 def graph(x_range,y_vals):
     # plot our list in X,Y coordinates
@@ -19,6 +23,16 @@ def preproc_data():
     X=df.iloc[:,:-1].values
     y=df.iloc[:,-1].values
     X_train, X_test, y_train, y_test = train_test_split(X,y,test_size=.2,random_state=0)
+    #need to simplify data so that there are no key errors during testing due to real val nums
+    for i in range(len(X_train)):
+        for j in range(len(X_train[i])):
+            X_train[i][j] = math.ceil(X_train[i][j])
+            
+
+    for i in range(len(X_test)):
+        for j in range(len(X_test[i])):
+            X_test[i][j] = math.ceil(X_test[i][j])
+
     y_sums = {"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0}
 
     for num in y_train:
@@ -33,7 +47,13 @@ def preproc_data():
     
     return X_train,X_test,y_train,y_test,y_sums
 
+
 def calc_prob_table(X_train, y_sums, y_train):
+    """
+    This function calculates the probabilities needed to compute and classify using NB
+    This is the training function
+    @returns P(Xi|Yk), P(Yk) 
+    """
     total_rows = len(X_train)
     prob_y = {"0":0,"1":0,"2":0,"3":0,"4":0,"5":0,"6":0,"7":0,"8":0,"9":0,"10":0}
     
@@ -63,15 +83,55 @@ def calc_prob_table(X_train, y_sums, y_train):
             prob_xy[p_xstr.format(i,y)]["{}".format(X_train[j][i-1])] /= y_sums[str(y)]
     
     return prob_xy, prob_y # the full probability tables of P(Yk) and P(Xi|Yk)
+
+def calc_new_y(prob_xy, prob_y, Xi_list, y):
+    """
+    returns the probability of a single P(Yk) PI P(Xi|Yk) for the argmax function
+    @returns P(Yk) PI P(Xi|Yk)
+    """
+    p_xstr = "p_x{}y{}"
+    pi_k = prob_y[str(y)]
+    theta_ijk = 1
+    i=1
+    try:
+        #print(Xi_list)
+        for elem in Xi_list:
+            #print(p_xstr.format(i,y),prob_xy[p_xstr.format(i,y)]," trying to access {}".format(elem))
+            theta_ijk *= prob_xy[p_xstr.format(i,y)][str(elem)]
+            i+=1
+
+        return pi_k * theta_ijk
+    except(KeyError):
+        return .00001
+
+
+
+def NB_test(prob_xy, prob_y, X_test, y_test):
+    """
+    Tests the accuracy of the NB model
+    @return Observed Y, Actual Y
+    """
+    y_list = []
+    for y in range(0,11): #max Yk is Y10
+        y_list.append(calc_new_y(prob_xy, prob_y, X_test, y))
     
+    print(y_list)
+    #need to argmax here
+    obs_y = max(y_list)
+    act_y = y_test
+    return obs_y, act_y
 
-
-
-    
 def NBayes():
     X_train,X_test,y_train,y_test,y_sums = preproc_data()
     prob_xy, prob_y = calc_prob_table(X_train, y_sums, y_train)
-    p_xstr = "p_x{}y{}"
+    for i in range(len(y_test)):
+        res = NB_test(prob_xy,prob_y,X_test[i],y_test[i])
+        p_error = np.abs(res[0]-res[1])/res[1]
+        acc = 1 - p_error
+        #print(acc)
+    
+
+
 
 
 
--- homework/H1/h1p2.py
+++ homework/H1/h1p2.py
@@ -14,10 +14,10 @@ def graph():
     ax = plt.axes(projection ='3d')
     
     # defining all 3 axes
-    x = np.arange(0,1,.01,dtype=float)
+    x = np.arange(0, 1, .01,dtype=float)
     y = np.arange(0, 1, .01,dtype=float)
-    z = (math.factorial(a+b+c)/(math.factorial(a)+math.factorial(b)+math.factorial(c)))*(np.power(x,a))*(np.power(y,b)) * (np.power((1-x-y),c)) #put L(theta) here
-    
+    z = (np.power(x,a))*(np.power(y,b)) * (np.power((1-x)*(1-y),c)) #put L(theta) here
+    #(math.factorial(a+b+c)/(math.factorial(a)+math.factorial(b)+math.factorial(c)))*
     # plotting
     ax.plot3D(x, y, z, 'green')
     ax.set_title('L(theta)')
