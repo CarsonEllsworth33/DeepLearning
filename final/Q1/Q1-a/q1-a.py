3efa8c8eb66b566ceb69ed2fb769e214b2ccc3e0
Carson Ellsworth <ellsc33@yahoo.com>
2022-05-02 13:58:38 -0500

    added key constants for the unpickled structure

--- final/Q1/Q1-a/q1-a.py
+++ final/Q1/Q1-a/q1-a.py
@@ -1,7 +1,19 @@
 import matplotlib as plt
 
+KEY_BATCH_LABELS = b'batch_label'
+KEY_LABELS = b'labels'   
+KEY_DATA = b'data'     
+KEY_FILENAMES = b'filenames'
+
 def unpickle(file):
     import pickle
     with open(file, 'rb') as fo:
         dict = pickle.load(fo, encoding='bytes')
     return dict
+
+data_dict = unpickle("../data/data_batch_1")
+
+
+for key in data_dict:
+    print(key)
+print(data_dict[KEY_DATA].shape)
